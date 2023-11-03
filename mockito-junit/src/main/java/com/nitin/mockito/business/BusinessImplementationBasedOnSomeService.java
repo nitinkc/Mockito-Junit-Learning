3f67e5bf133bf66e70428ddac7c0a2742bfe5268
Nitin Chaurasia <gs.nitin@gmail.com>
2023-11-02 23:54:15 -0600

    modularization

--- mockito-junit/src/main/java/com/nitin/mockito/business/BusinessImplementationBasedOnSomeService.java
+++ mockito-junit/src/main/java/com/nitin/mockito/business/BusinessImplementationBasedOnSomeService.java
@@ -2,6 +2,7 @@ package com.nitin.mockito.business;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -30,5 +31,23 @@ public class BusinessImplementationBasedOnSomeService {
 				.collect(Collectors.toList());
 		
 		return filteredStrings; 
+	}
+
+	public List<Map<String,Objects>> myMethod(String str){
+
+		//Make a DB Call with str say for ex Last name
+		List<String> namesWithSameLastName = someService.retrieveStringsFromDbOrService(str);//Mock this
+
+		//Find the meaning of the name from MeanongFinerService with Rest Template call
+
+		//https://run.mocky.io/v3/bbb214bf-5245-4549-9219-c016607ce75f
+		//Delete: https://designer.mocky.io/manage/delete/bbb214bf-5245-4549-9219-c016607ce75f/L8K9BQSs5FK8SuXObIKgVP9GGo9v0tA4C7iY
+
+		List<Map<String,Objects>> response = new RestTemplate.exchange();//Mock This as well.
+
+
+		namesWithSameLastName.get(0)
+
+
 	}
 }
